5e633db420e12c1fe78f2604eea696749370e195
Glenn Siegman <gsiegman@gsiegman.com>
2011-05-09 18:56:36 -0400

    added a setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+description = "A Facebook Python client."
+
+VERSION = 'pre-0.1'
+
+setup(
+    name='python-facebook',
+    version=VERSION,
+    author='Glenn Siegman',
+    author_email='gsiegman@gsiegman.com',
+    url='https://github.com/gsiegman/python-facebook',
+    description=description,
+    long_description=description,
+    license='BSD',
+    platforms=['any',],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ],
+    packages=['facebook',],
+    install_requires=['requests',]
+)
